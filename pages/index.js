b7cf6df301629ea3c640dd0b850774733582afd8
JoseMTrias <jmgarciatrias@gmail.com>
2023-02-21 10:37:31 +0100

    adding file structure

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Navigation from "../components/Navigation";
+import Pieces from "./pieces";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <Pieces />
+    </>
   );
 }
---
+++ pages/pieces/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import id from "./id/[id]";
+
+export default function Pieces() {
+  return <div>Pieces from pieces folder</div>;
+}
