8388a34d0e30f00359652cc6c5f88460bea0dec8
AishwaryaKamble96 <aishwaryakamblegem@gmail.com>
2023-02-21 12:17:43 +0100

    inital spotlight compo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Navigation from "../components/Navigation";
+import Spotlight from "../components/Spotlight";
 import Pieces from "./pieces";
 
 export default function HomePage() {
   return (
     <>
       <Pieces />
+      <Spotlight />
     </>
   );
 }
