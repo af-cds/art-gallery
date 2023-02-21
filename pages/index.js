adeb037f1fbc6271986af905b4fa5797e06bba9a
JoseMTrias <jmgarciatrias@gmail.com>
2023-02-21 10:59:39 +0100

    fetch done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Navigation from "../components/Navigation";
 import Pieces from "./pieces";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function HomePage() {
+  const { data, error, isLoading, isValidating } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+
   return (
     <>
-      <Pieces />
+      <div>
+        <span role="img" aria-label={isValidating ? "Validating" : "Ready"}>
+          {isValidating ? "🔄" : "✅"}
+        </span>
+        Hello {data.name}!
+      </div>
     </>
   );
 }
