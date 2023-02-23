31e5c7baf019ddf06ef04a5fe428a0223dedd117
af-cds <adrian.fricke@gmail.com>
2023-02-23 09:39:33 +0100

    final styling for iphone SE

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,7 @@ export default function App({ Component, pageProps }) {
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
 
+  
   return (
     <>
       <GlobalStyle />
